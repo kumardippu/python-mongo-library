bd0f86aad40632647a623b7fd4431c03b55fb9ab
Dippu Kumar <kumardippu@gmail.com>
2022-06-10 11:33:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+## Developed By : Dippu Kumar
+## Developed for MongoDB
+## Developed on : 09-06-2022
+
 HOST = "localhost"
 USERNAME= "<YOUR_MONGODB_USER_NAME"
 PASSWORD = "<YOUR_MONGODB_PASSWORD>"
